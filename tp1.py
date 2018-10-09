49cdf646931485070029606b852da9c391e9b24e
Santiaso <santiago.aso@trocafone.com>
2018-10-08 22:17:50 -0300

    SOR funcionando para n= [5, 10, 100]

--- tp1.py
+++ tp1.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 L = 1
 E_I = 1
+nes = [5, 10, 100]
 
 def SOR(A, b, Imax, eps, x0, omega):
     D = np.diag(np.diag(A))
@@ -20,60 +21,106 @@ def SOR(A, b, Imax, eps, x0, omega):
         err = la.norm(r,2)
         res.append(err)
         i = i+1
+    print("n = " + str(len(b)) + " converge en " + str(i) + " pasos")
     return (x,i,res)
 
-def CreateMatrixK():
-	matrixK = np.array([
-		[1,0,0,0,0,0,0,0,0,0,0,0],
-		[-4,5,-4,1,0,0,0,0,0,0,0,0],
-		[1,-4,6,-4,1,0,0,0,0,0,0,0],
-		[0,1,-4,6,-4,1,0,0,0,0,0,0],
-		[0,0,1,-4,6,-4,1,0,0,0,0,0],
-		[0,0,0,1,-4,6,-4,1,0,0,0,0],
-		[0,0,0,0,1,-4,6,-4,1,0,0,0],
-		[0,0,0,0,0,1,-4,6,-4,1,0,0],
-		[0,0,0,0,0,0,1,-4,6,-4,1,0],
-		[0,0,0,0,0,0,0,1,-4,6,-4,1],
-		[0,0,0,0,0,0,0,0,1,-4,5,-4],
-		[0,0,0,0,0,0,0,0,0,0,0,1]
-		])
+def CreateLineMatrixK(j, n):
+	if (j == 1):
+		newLine = [1]
+		i = 1
+		while (i < n):
+			newLine.append(0)
+			i = i + 1
+		return newLine
+	if (j == 2):
+		newLine = [-4, 5, -4, 1]
+		i = 4
+		while (i < n):
+			newLine.append(0)
+			i = i + 1
+		return newLine
+	if (j >= 3 and j <= (n-2)):
+		newLine = []
+		i = 0
+		while (i < (j-3)):
+			newLine.append(0)
+			i = i + 1
+		newLine.append(1)
+		newLine.append(-4)
+		newLine.append(6)
+		newLine.append(-4)
+		newLine.append(1)
+		i = i + 5
+		while (i < n):
+			newLine.append(0)
+			i = i + 1
+		return newLine
+	if (j == (n-1)):
+		newLine = []
+		i = 0
+		while (i < (n - 4)):
+			newLine.append(0)
+			i = i + 1
+		newLine.append(1)
+		newLine.append(-4)
+		newLine.append(5)
+		newLine.append(-4)
+		i = i + 4
+		return newLine	
+	if (j == (n)):
+		newLine = []
+		i = 0
+		while (i < (n - 1)):
+			newLine.append(0)
+			i = i + 1
+		newLine.append(1)
+		return newLine
+
+def CreateMatrixK(n):
+	matrixK = []
+	i = 1
+	while (i <= n):
+		newLine = CreateLineMatrixK(i,n)
+		matrixK.append(newLine)
+		i = i + 1 
 	return matrixK
 
 def CreateVectorF(n):
-	vectorF = np.array(0)
+	vectorF = []
+	vectorF.append(0)
 	i = 1
-	while (i < n):
+	while (i < n-1):
 		newElement = (((18 + (18**2) * ((i*L/n)-(i*L/n)**2))/E_I ) * ((L/n)**4))
-		vectorF = np.append(vectorF, newElement)
+		vectorF.append(newElement)
 		i = i + 1 
-	vectorF = np.append(vectorF, 0)
+	vectorF.append(0)
 	return vectorF
 
 def CreateVectorU(n):
-	vectorU = np.array(0)
+	vectorU = []
+	vectorU.append(0)
 	i = 1
-	while (i <= n):
-		newElement = 0
-		vectorU = np.append(vectorU, newElement)
+	while (i < n):
+		newElement = 1
+		vectorU.append(newElement)
 		i = i + 1
 	return vectorU
 
-n = 11
-K = CreateMatrixK()
-u = CreateVectorU(n)  
-f = CreateVectorF(n)
-print(K)
-print(u)
-print(f)
+x = 0
+while (x < len(nes)):
+	K = CreateMatrixK(nes[x])
+	u = CreateVectorU(nes[x])
+	f = CreateVectorF(nes[x])
 
-def Graph():
-	i = 1.0
-	while (i < 2):
-	    [x,i,res] = SOR(K,f,100,10**(-3),u,i)
-	    plt.plot(range(len(res)),res)
-	    plt.yscale('log')
-	    plt.show()
-	    i = i + 0.05
+	def Graph():
+		i = 1.0
+		while (i < 2):
+		    [x,i,res] = SOR(K, f, 10000, 10**(-2), u, i)
+		    plt.plot(range(len(res)),res)
+		    plt.yscale('log')
+		    plt.show()
+		    i = i + 0.05
 
-Graph()
+	Graph()
+	x = x + 1
 
