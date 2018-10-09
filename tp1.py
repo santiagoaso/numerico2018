f60bbfa15c4b4ca997756e71e0dc399f749a3b77
Santiaso <santiago.aso@trocafone.com>
2018-10-08 23:43:35 -0300

    Puntos 3, 4, 5, 6

--- tp1.py
+++ tp1.py
@@ -5,24 +5,28 @@ import matplotlib.pyplot as plt
 L = 1
 E_I = 1
 nes = [5, 10, 100]
+nro_grupo = 18
 
-def SOR(A, b, Imax, eps, x0, omega):
-    D = np.diag(np.diag(A))
-    M = np.dot((1/omega), D) + np.tril(A, -1)
-    N = M - A
-    r = np.dot(A,x0)-b
-    x = x0
+def SOR(K, f, Imax, eps, u, omega):
+    D = np.diag(np.diag(K))
+    M = np.dot((1 / omega), D) + np.tril(K, -1)
+    N = M - K
+    r = np.dot(K, u) - f
+    x = u
     i = 0
     err = 1 + eps
     res = []
-    while((i < Imax) and ((la.norm(r)) >= eps)):
-        x = np.dot(np.dot((la.inv(M)),N),x) + np.dot((la.inv(M)),b)
-        r = np.dot(A,x) - b
-        err = la.norm(r,2)
+    cut = err
+    while((i < Imax) and (cut >= eps)):
+        x_old = x
+        x = np.dot(np.dot((la.inv(M)), N), x) + np.dot((la.inv(M)), f)
+        r = np.dot(K, x) - f
+        err = la.norm(r, np.inf)
         res.append(err)
-        i = i+1
-    print("n = " + str(len(b)) + " converge en " + str(i) + " pasos")
-    return (x,i,res)
+        cut = (la.norm(x - x_old, np.inf)) / (la.norm(x, np.inf))
+        i = i + 1
+    # print("n = " + str(len(f)) + " converge en " + str(i) + " pasos para omega: " + str(omega))
+    return (x, i, res)
 
 def CreateLineMatrixK(j, n):
 	if (j == 1):
@@ -90,7 +94,7 @@ def CreateVectorF(n):
 	vectorF.append(0)
 	i = 1
 	while (i < n-1):
-		newElement = (((18 + (18**2) * ((i*L/n)-(i*L/n)**2))/E_I ) * ((L/n)**4))
+		newElement = (((nro_grupo + (nro_grupo**2) * ((i * L/n) - (i * L/n)**2)) / E_I ) * ((L/n)**4))
 		vectorF.append(newElement)
 		i = i + 1 
 	vectorF.append(0)
@@ -108,19 +112,35 @@ def CreateVectorU(n):
 
 x = 0
 while (x < len(nes)):
-	K = CreateMatrixK(nes[x])
-	u = CreateVectorU(nes[x])
-	f = CreateVectorF(nes[x])
+	current_n = nes[x]
+	K = CreateMatrixK(current_n)
+	u = CreateVectorU(current_n)
+	f = CreateVectorF(current_n)
 
-	def Graph():
-		i = 1.0
-		while (i < 2):
-		    [x,i,res] = SOR(K, f, 10000, 10**(-2), u, i)
-		    plt.plot(range(len(res)),res)
-		    plt.yscale('log')
-		    plt.show()
-		    i = i + 0.05
+	def CalculateBestOmega():
+		l = 1.0
+		bestOmega = 0
+		recordSteps = 999
+		while (l < 2):
+		    [x, i, res] = SOR(K, f, 10000, 10**(-2), u, l)
+		    if (len(res) < recordSteps):
+		    	bestOmega = l
+		    	recordSteps = len(res)
+		    l = l + 0.05
+		return [bestOmega, recordSteps]
 
-	Graph()
+	[bestOmega, recordSteps] = CalculateBestOmega()
+
+	def Graph(omega):
+		[x, i, res] = SOR(K, f, 10000, 10**(-4), u, omega)
+		plt.plot(range(len(res)),res)
+		plt.title("n = " + str(current_n) + ". Omega = " + str(bestOmega) + ". Steps: " + str(len(res)))
+		plt.yscale('log')
+		plt.ylabel('Infinity Norm (log)')
+		plt.xlabel('Step')
+		plt.show()
+
+	Graph(bestOmega)
+
 	x = x + 1
 
